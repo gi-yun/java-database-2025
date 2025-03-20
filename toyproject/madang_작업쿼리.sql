cfd7fff463a1e8197ace103342f6c87183ba3796
gi-yun <kbb9554@gmail.com>
2025-03-20 17:11:26 +0900

    8일차 마무리 커밋

--- toyproject/madang_작업쿼리.sql
+++ toyproject/madang_작업쿼리.sql
@@ -9,3 +9,10 @@ INSERT INTO STUDENTS(STD_ID, STD_NAME, STD_MOBILE, STD_REGYEAR)
 VALUES (SEQ_STUDENT.nextval, '홍길동', '010-9999-8888', 1997);
 INSERT INTO STUDENTS(STD_ID, STD_NAME, STD_MOBILE, STD_REGYEAR) 
 VALUES (SEQ_STUDENT.nextval, '홍길순', '010-9999-8877', 2000);
+
+SELECT s.std_id, s.std_name
+	 , s.STD_MOBILE , s.STD_REGYEAR
+ FROM STUDENTS s
+ 
+ INSERT INTO MADANG.STUDENTS (std_id, std_name, std_mobile, std_regyear)
+VALUES(SEQ_STUDENT.NEXTVAL, :v_std_name, :v_std_mobile, :v_std_regyear);
--- toyproject/ora01_test.py
+++ toyproject/ora01_test.py
@@ -11,17 +11,18 @@ port = 1521
 username = 'madang'
 password = 'madang'
 
-# DB연결 시작
-conn = oci.connect(f'{username}/{password}@{host}:{port}/{sid}')
-cursor = conn.cursor() # DB 커서와 동일한 역할을 하는 개체
+class Main():
+    # DB연결 시작
+    conn = oci.connect(f'{username}/{password}@{host}:{port}/{sid}')
+    cursor = conn.cursor() # DB 커서와 동일한 역할을 하는 개체
 
-query = 'SELECT * FROM Students' # 파이썬에서는 쿼리호출시는 ; 삭제
-cursor.execute(query)
+    query = 'SELECT * FROM Students' # 파이썬에서는 쿼리호출시는 ; 삭제
+    cursor.execute(query)
 
-# 불러온 데이터처리
-for i, item in enumerate(cursor, start=1):
-    print(item)
+    # 불러온 데이터처리
+    for i, item in enumerate(cursor, start=1):
+        print(item)
 
-cursor.close()
-conn.close()
+    cursor.close()
+    conn.close()
 # DB는 연결하면 마지막에 close(), 파일은 오픈하면 마지막에 닫아줘야 함.
